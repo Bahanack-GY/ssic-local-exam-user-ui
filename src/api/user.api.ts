a3d77a54ed05955714a897e8e7f2b0f4e7d2eab2
unknown <georgesyvan12@gmail.com>
2025-05-05 18:05:09 +0100

    fixed the ip

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.16.103:3030/api/v1', // Add http://!
+    baseURL: 'http://192.168.16.105:3030/api/v1', // Add http://!
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
