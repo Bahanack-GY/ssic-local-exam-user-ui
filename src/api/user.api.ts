0c9c913cfab40b7b5381c692d32126f700e5c608
unknown <georgesyvan12@gmail.com>
2025-05-06 18:01:26 +0100

    changed ip for production

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.16.105:3030/api/v1', // Add http://!
+    baseURL: 'http://192.168.1.238:3030/api/v1', 
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
